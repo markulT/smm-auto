be9530aeff0bb625fc8523ac52f45ff23ea525b8
Marco Bobr <marrcokyr@gmail.com>
2023-08-27 22:49:52 +0300

    telegram api init

--- utils/auth/authMiddleware.go
+++ utils/auth/authMiddleware.go
@@ -2,22 +2,20 @@ package auth
 
 import (
 	"github.com/gin-gonic/gin"
-	"os"
-	"strings"
 )
 
 func AuthMiddleware(c *gin.Context) gin.HandlerFunc {
 	return func(context *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-			c.JSON(401, gin.H{"error":"Unauthorized"})
-			c.Abort()
-			return
-		}
-		accessToken := authHeader[7:]
-		if _, err := Validate(accessToken, os.Getenv("secretKey"));err!=nil {
-			c.JSON(401, gin.H{"error":"Invalid token"})
-		}
+		//authHeader := c.GetHeader("Authorization")
+		//if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+		//	c.JSON(401, gin.H{"error":"Unauthorized"})
+		//	c.Abort()
+		//	return
+		//}
+		//accessToken := authHeader[7:]
+		//if _, err := Validate(accessToken, os.Getenv("secretKey"));err!=nil {
+		//	c.JSON(401, gin.H{"error":"Invalid token"})
+		//}
 		c.Next()
 	}
 }
